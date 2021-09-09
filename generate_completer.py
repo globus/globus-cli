746e28a8c127bd5818276083ea1ac25f0850a582
Stephen Rosen <sirosen@globus.org>
2021-09-09 02:39:19 +0000

    Various fixes to ZSH completer
    Importantly but subtly, the `*::args->args` and subcommand matching
    specs need to specify `(-)` as a prefix option, meaning that the match
    on an argument is considered mutually exclusive with further
    processing on CLI options. This allows the subcommand to pick up on
    its own options correctly.

--- generate_completer.py
+++ generate_completer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """completion generator for globus-cli"""
 import contextlib
+import re
 import sys
 import textwrap
 
@@ -8,6 +9,8 @@ import click
 from pkg_resources import load_entry_point
 
 CLI = load_entry_point("globus-cli", "console_scripts", "globus")
+
+_ZSH_HELP_ESC_RE = re.compile(r'([":\[\]])')
 
 
 def walk_contexts(name="globus", cmd=CLI, parent_ctx=None):
@@ -26,10 +29,10 @@ def walk_contexts(name="globus", cmd=CLI, parent_ctx=None):
         if subcmd.hidden:
             continue
 
-        cmds.append(click.Context(subcmd, info_name=subcmdname, parent=current_ctx))
-
         if isinstance(subcmd, click.Group):
             groups.append(walk_contexts(subcmdname, subcmd, current_ctx))
+        else:
+            cmds.append(click.Context(subcmd, info_name=subcmdname, parent=current_ctx))
 
     return (current_ctx, cmds, groups)
 
@@ -42,8 +45,12 @@ def iter_all_contexts(tree=None):
         yield from iter_all_contexts(g)
 
 
+def is_repeatable(o):
+    return o.count or o.multiple
+
+
 def compute_nargs(o):
-    if o.is_flag:
+    if o.is_flag or o.count:
         return 0
     else:
         return o.nargs
@@ -243,7 +250,6 @@ class BashCompleter(Completer):
 
     def print_completion(self):
         print(self.PROLOGUE)
-        self.print_data()
         for ctx in iter_all_contexts():
             if isinstance(ctx.command, click.Group):
                 self._print_group_info(ctx)
@@ -271,7 +277,8 @@ class ZshCompleter(Completer):
     def _in_completion_func(self, ctx):
         print(f"{self._cmd_completer_name(ctx)}() {{")
         if self._is_root_ctx(ctx):
-            print("  local line\n")
+            print('  local curcontext="$curcontext" context state state_descr line')
+            print("  typeset -A opt_args\n")
         yield
         print("}")
 
@@ -287,26 +294,51 @@ class ZshCompleter(Completer):
 
     @contextlib.contextmanager
     def _in_line_case(self, ctx):
-        print("  case $line[1] in")
+        print("  case $state in")
+        print("    args)")
+        print("      case $line[1] in")
         yield
+        print("      esac")
+        print("    ;;")
         print("  esac")
 
     def _option_descs(self, o):
         nargs = compute_nargs(o)
-        spec = ""
+        argspec = ""
         if isinstance(o.type, click.Choice):
-            spec = ": :(" + " ".join(o.type.choices) + ")"
+            argspec = ": :(" + " ".join(o.type.choices) + ")"
         elif nargs > 0:
-            spec = ": :( )"
+            argspec = ": :( )"
 
-        helptext = f"[{o.help}]".replace('"', '\\"')
-        # TODO: determine if we need '=[...]' in our spec
-        # if nargs > 0:
-        #     helptext = f"={helptext}"
+        # escape quotes and colons
+        helptext = "[" + _ZSH_HELP_ESC_RE.sub(r"\\\1", o.help) + "]"
 
-        flags = opt_strs(o)
-        for flag in flags:
-            yield f'"({flag}){flag}{helptext}{spec}"'
+        # TODO: add mutual exclusivity info from mutexinfo
+        # when options are listed in the parenthetical section at the start of an option
+        # spec, they are noted as mutually exclusive
+        # here we list aliases, so that `--help` and `-h` are considered mutex (for
+        # better completion)
+        raw_flags = flags = opt_strs(o)
+
+        # append a + if a flag takes one argument and is a short option
+        # this indicates that we support option-slamming, as in `-Fjson`
+        flags = [
+            (f"{flag}+" if len(flag) == 2 and nargs == 1 else flag) for flag in flags
+        ]
+        # append an = if a flag takes one argument and is a long option
+        # this indicates that we support `--format=text`
+        flags = [
+            (f"{flag}=" if flag.startswith("--") and nargs == 1 else flag)
+            for flag in flags
+        ]
+        if is_repeatable(o):
+            flags = [f"*{flag}" for flag in flags]
+
+        if len(flags) == 1:
+            yield f'"{flags[0]}{helptext}{argspec}"'
+        else:
+            for flag in flags:
+                yield ('"(' + " ".join(raw_flags) + ")" + f'{flag}{helptext}{argspec}"')
 
     def _all_option_descs(self, ctx):
         options = [
@@ -332,15 +364,27 @@ class ZshCompleter(Completer):
             print("  _arguments -C \\")
             for desc in self._all_option_descs(ctx):
                 print(f"    {desc} \\")
-            print(f'    "1: :{self._subcmd_describer_name(ctx)}" \\')
-            print('    "*::arg:->args"\n')
+            # IMPORTANT!
+            # This is subtle, but each of these specs MUST have '(-)', indicating mutual
+            # exclusivity with additional CLI options
+            # This prevents the higher-level command in a tree from eagerly consuming
+            # options which follow subcommands
+            #
+            # For example, if the input command is `globus endpoint -h`, we *don't* want
+            # the `_arguments` call to consume `-h` as an option to the `globus` command
+            # It should instead be left un-parsed, and the subsequent `_arguments` call
+            # for the `endpoint` subcommand will pick it up correctly
+            #
+            # This behavior has been tested and does not mark these matches as mutually
+            # exclusive with prior options. Meaning that `globus -Fjson <TAB>` will
+            # run the subcommand description action as desired
+            print(f'    "(-): :{self._subcmd_describer_name(ctx)}" \\')
+            print('    "(-)*::arg:->args"\n')
 
             with self._in_line_case(ctx):
                 for n in subcommand_names:
                     funcname = f"{self._cmd_completer_name(ctx)}_{n.replace('-', '_')}"
-                    print(f'    "{n}")')
-                    print(f"      {funcname}")
-                    print("      ;;")
+                    print(f'        "{n}") {funcname} ;;')
 
     def print_completion(self):
         for ctx in iter_all_contexts():
@@ -351,15 +395,20 @@ class ZshCompleter(Completer):
         print(self.EPILOGUE)
 
 
-def print_data(shell):
-    if shell.lower() == "zsh":
-        ZshCompleter().print_completion()
+def main():
+    if len(sys.argv) > 1:
+        shell = sys.argv[1].lower()
     else:
-        BashCompleter().print_completion()
+        shell = "bash"
+
+    completer_cls = {
+        "zsh": ZshCompleter,
+        "zshell": ZshCompleter,
+        "bash": BashCompleter,
+    }.get(shell, BashCompleter)
+
+    completer_cls().print_completion()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print_data(sys.argv[1])
-    else:
-        print_data("bash")
+    main()
