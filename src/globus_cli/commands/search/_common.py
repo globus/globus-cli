ab206ceeca8fc8231bc74fed11286b7fd320320f
Stephen Rosen <sirosen@globus.org>
2022-10-25 17:20:34 +0000

    Convert Search principal rendering to a formatter
    This is a formatter which handles the passing of an AuthClient,
    aggregation of data into an IdentityMap, lookups in the map, and
    various ways of formatting the different principal types.
    
    It probably doesn't match what other commands are doing, but it could
    grow into a replacement for those usages in the future.

--- src/globus_cli/commands/search/_common.py
+++ src/globus_cli/commands/search/_common.py
@@ -5,7 +5,7 @@ import typing as t
 import click
 import globus_sdk
 
-from globus_cli.termio import Field
+from globus_cli.termio import Field, field_formatters
 
 
 def index_id_arg(f: t.Callable) -> t.Callable:
@@ -16,35 +16,58 @@ def task_id_arg(f: t.Callable) -> t.Callable:
     return click.argument("task_id", metavar="TASK_ID", type=click.UUID)(f)
 
 
+class _PrincipalFormatter(field_formatters.FieldFormatter[t.Tuple[str, str, str]]):
+    def __init__(
+        self,
+        auth_client: globus_sdk.AuthClient,
+        *,
+        type_key: str = "principal_type",
+        value_key: str = "principal",
+    ) -> None:
+        self.type_key = type_key
+        self.value_key = value_key
+        self.resolved_ids = globus_sdk.IdentityMap(auth_client)
+
+    def add_items(self, items: t.Iterable[dict]) -> None:
+        for x in items:
+            if x.get(self.type_key) != "identity":
+                continue
+            self.resolved_ids.add(t.cast(str, x[self.value_key]).split(":")[-1])
+
+    def parse(self, value: t.Any) -> tuple[str, str, str]:
+        if not isinstance(value, dict):
+            raise ValueError("cannot format principal from non-dict data")
+
+        unparsed_principal = t.cast(str, value[self.value_key])
+
+        return (
+            value[self.type_key],
+            unparsed_principal.split(":")[-1],
+            unparsed_principal,
+        )
+
+    def render(self, value: tuple[str, str, str]) -> str:
+        ptype, pvalue, unparsed = value
+        if ptype == "identity":
+            try:
+                return t.cast(str, self.resolved_ids[pvalue]["username"])
+            except LookupError:
+                return pvalue
+        elif ptype == "group":
+            return f"Globus Group ({pvalue})"
+        else:
+            return unparsed
+
+
 def resolved_principals_field(
     auth_client: globus_sdk.AuthClient,
     items: t.Iterable[dict[str, t.Any]] | None = None,
-    *,
-    name: str = "Principal",
-    type_key: str = "principal_type",
-    value_key: str = "principal",
 ) -> Field:
-    resolved_ids = globus_sdk.IdentityMap(
-        auth_client,
-        (x[value_key].split(":")[-1] for x in items if x[type_key] == "identity")
-        if items
-        else [],
-    )
+    formatter = _PrincipalFormatter(auth_client)
+    if items is not None:
+        formatter.add_items(items)
 
-    def render_principal(item: dict[str, t.Any]) -> str:
-        value = item[value_key].split(":")[-1]
-        if item[type_key] == "identity":
-            try:
-                ret = resolved_ids[value]["username"]
-            except LookupError:
-                ret = value
-        elif item[type_key] == "group":
-            ret = f"Globus Group ({value})"
-        else:
-            ret = item[value_key]
-        return str(ret)
-
-    return Field(name, render_principal)
+    return Field("Principal", "@", formatter=formatter)
 
 
 INDEX_FIELDS = [
