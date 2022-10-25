7a8ee36da06534ac6143ae6973819eb586100bb2
Stephen Rosen <sirosen@globus.org>
2022-10-25 06:53:33 +0000

    Convert transfer ACL rendering to custom formatter

--- src/globus_cli/commands/endpoint/permission/show.py
+++ src/globus_cli/commands/endpoint/permission/show.py
@@ -1,8 +1,41 @@
+from __future__ import annotations
+
+import typing as t
+import uuid
+
 import click
 
 from globus_cli.login_manager import LoginManager
 from globus_cli.parsing import command, endpoint_id_arg
-from globus_cli.termio import FORMAT_TEXT_RECORD, Field, formatted_print
+from globus_cli.termio import (
+    FORMAT_TEXT_RECORD,
+    Field,
+    field_formatters,
+    formatted_print,
+)
+
+if t.TYPE_CHECKING:
+    from globus_cli.services.auth import CustomAuthClient
+
+
+class AclPrincipalFormatter(field_formatters.FieldFormatter[t.Tuple[str, str]]):
+    def __init__(self, auth_client: CustomAuthClient):
+        self.auth_client = auth_client
+
+    def parse(self, value: t.Any) -> tuple[str, str]:
+        if not isinstance(value, dict):
+            raise ValueError("bad value for ACL principal, not a dict")
+
+        return (str(value.get("principal_type")), str(value.get("principal")))
+
+    def render(self, value: tuple[str, str]) -> str:
+        principal_type, principal = value
+        if principal_type == "identity":
+            return str(self.auth_client.lookup_identity_name(principal))
+        elif principal_type == "group":
+            return f"https://app.globus.org/groups/{principal}"
+        else:
+            return principal_type
 
 
 @command(
@@ -19,20 +52,12 @@ $ globus endpoint permission show $ep_id $rule_id
 @endpoint_id_arg
 @click.argument("rule_id")
 @LoginManager.requires_login(LoginManager.AUTH_RS, LoginManager.TRANSFER_RS)
-def show_command(*, login_manager: LoginManager, endpoint_id, rule_id):
+def show_command(*, login_manager: LoginManager, endpoint_id: uuid.UUID, rule_id: str):
     """
     Show detailed information about a single access control rule on an endpoint.
     """
     transfer_client = login_manager.get_transfer_client()
     auth_client = login_manager.get_auth_client()
-
-    def _shared_with_keyfunc(rule):
-        if rule["principal_type"] == "identity":
-            return auth_client.lookup_identity_name(rule["principal"])
-        elif rule["principal_type"] == "group":
-            return "https://app.globus.org/groups/{}".format(rule["principal"])
-        else:
-            return rule["principal_type"]
 
     rule = transfer_client.get_endpoint_acl_rule(endpoint_id, rule_id)
     formatted_print(
@@ -41,7 +66,11 @@ def show_command(*, login_manager: LoginManager, endpoint_id, rule_id):
         fields=[
             Field("Rule ID", "id"),
             Field("Permissions", "permissions"),
-            Field("Shared With", _shared_with_keyfunc),
+            Field(
+                "Shared With",
+                "@",
+                formatter=AclPrincipalFormatter(auth_client=auth_client),
+            ),
             Field("Path", "path"),
         ],
     )
