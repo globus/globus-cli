c7f86e18d311939840c01f7a8d1e15ccb90dc45e
Stephen Rosen <sirosen@globus.org>
2022-10-25 17:27:35 +0000

    Convert ep role show rendering to formatter
    The `globus endpoint role show` command has a much reduced version of
    the principal resolution problem, in this case through the
    CustomAuthClient and not through an IdentityMap.
    
    Rather than trying to unify the two implementations, start by
    introducing this separate standalone version.

--- src/globus_cli/commands/endpoint/role/show.py
+++ src/globus_cli/commands/endpoint/role/show.py
@@ -1,8 +1,28 @@
+from __future__ import annotations
+
+import typing as t
+
 from globus_cli.login_manager import LoginManager
 from globus_cli.parsing import command, endpoint_id_arg
-from globus_cli.termio import FORMAT_TEXT_RECORD, Field, formatted_print
+from globus_cli.termio import (
+    FORMAT_TEXT_RECORD,
+    Field,
+    field_formatters,
+    formatted_print,
+)
 
 from ._common import role_id_arg
+
+if t.TYPE_CHECKING:
+    from globus_cli.services.auth import CustomAuthClient
+
+
+class _SimplePrincipalFormatter(field_formatters.StrFieldFormatter):
+    def __init__(self, auth_client: CustomAuthClient) -> None:
+        self.auth_client = auth_client
+
+    def render(self, value: str) -> str:
+        return str(self.auth_client.lookup_identity_name(value))
 
 
 @command(
@@ -41,16 +61,17 @@ def role_show(*, login_manager: LoginManager, endpoint_id, role_id):
     transfer_client = login_manager.get_transfer_client()
     auth_client = login_manager.get_auth_client()
 
-    def lookup_principal(role):
-        return auth_client.lookup_identity_name(role["principal"])
-
     role = transfer_client.get_endpoint_role(endpoint_id, role_id)
     formatted_print(
         role,
         text_format=FORMAT_TEXT_RECORD,
         fields=[
             Field("Principal Type", "principal_type"),
-            Field("Principal", lookup_principal),
+            Field(
+                "Principal",
+                "principal",
+                formatter=_SimplePrincipalFormatter(auth_client),
+            ),
             Field("Role", "role"),
         ],
     )
