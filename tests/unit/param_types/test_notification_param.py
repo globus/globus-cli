eea8a58ea24bd03e4d0a1e0502a92d2b69d42131
Stephen Rosen <sirosen@globus.org>
2022-12-23 17:51:42 +0000

    Improve test coverage of notify param type

--- tests/unit/param_types/test_notification_param.py
+++ tests/unit/param_types/test_notification_param.py
@@ -7,7 +7,7 @@ from globus_cli.parsing.param_types import NotificationParamType
 
 @click.command()
 @click.option("--notify", type=NotificationParamType(), callback=none_to_empty_dict)
-def foo(notify):
+def notify_cmd(notify):
     assert isinstance(notify, dict)
     click.echo(f"len(notify)={len(notify)}")
     for k in sorted(notify):
@@ -16,14 +16,14 @@ def foo(notify):
 
 def test_notify_no_opts(runner):
     # no arg becomes empty dict
-    result = runner.invoke(foo, [])
+    result = runner.invoke(notify_cmd, [])
     assert result.exit_code == 0
     assert result.output == "len(notify)=0\n"
 
 
 @pytest.mark.parametrize("arg", ("", "off", "OFF", "Off"))
 def test_notify_opt_off(runner, arg):
-    result = runner.invoke(foo, ["--notify", arg])
+    result = runner.invoke(notify_cmd, ["--notify", arg])
     assert result.exit_code == 0
     assert (
         result.output
@@ -38,7 +38,7 @@ notify.notify_on_succeeded=False
 
 @pytest.mark.parametrize("arg", ("on", "ON", "On", "on,failed", "succeeded , on"))
 def test_notify_opt_on(runner, arg):
-    result = runner.invoke(foo, ["--notify", arg])
+    result = runner.invoke(notify_cmd, ["--notify", arg])
     assert result.exit_code == 0
     assert result.output == "len(notify)=0\n"
 
@@ -62,7 +62,7 @@ def test_notify_opt_on(runner, arg):
     ),
 )
 def test_notify_single_opt(runner, arg, failed_val, inactive_val, succeeded_val):
-    result = runner.invoke(foo, ["--notify", arg])
+    result = runner.invoke(notify_cmd, ["--notify", arg])
     assert result.exit_code == 0
     assert (
         result.output
@@ -77,14 +77,14 @@ notify.notify_on_succeeded={succeeded_val}
 
 def test_notify_unrecognized_opt(runner):
     # invalid opts get rejected
-    result = runner.invoke(foo, ["--notify", "whenever"])
+    result = runner.invoke(notify_cmd, ["--notify", "whenever"])
     assert result.exit_code == 2
     assert "--notify received these invalid values: ['whenever']" in result.output
 
 
 def test_notify_cannot_mix_opt_with_off(runner):
     # mixing off with other opts is rejected
-    result = runner.invoke(foo, ["--notify", "off,inactive"])
+    result = runner.invoke(notify_cmd, ["--notify", "off,inactive"])
     assert result.exit_code == 2
     assert '--notify cannot accept "off" and another value' in result.output
 
@@ -102,11 +102,24 @@ def test_notify_cannot_mix_opt_with_off(runner):
         (",,succeeded,", {"succeeded,failed", "succeeded,inactive"}),
         (",,succeeded,,f", {"succeeded,failed"}),
         (",", {"succeeded", "failed", "inactive"}),
+        ("succeeded,failed,inactive,", {"succeeded,failed,inactive"}),
+        (",,,succeeded,,,,failed,inactive,", {"succeeded,failed,inactive"}),
+        ("succeeded,UNKNOWN", {"succeeded,UNKNOWN"}),
+        (",,succeeded,UNKNOWN", {"succeeded,UNKNOWN"}),
     ),
 )
 def test_notify_shell_complete(runner, incomplete_value, expected_completions):
     param_type = NotificationParamType()
     param = click.Option(["--notify"], type=param_type)
-    completions = param_type.shell_complete(click.Context(foo), param, incomplete_value)
+    completions = param_type.shell_complete(
+        click.Context(notify_cmd), param, incomplete_value
+    )
     got_values = {c.value for c in completions}
     assert got_values == expected_completions
+
+
+def test_notify_metavar_in_help(runner):
+    # running `--help` should show the custom metavar for `--notify`
+    result = runner.invoke(notify_cmd, ["--help"])
+    assert result.exit_code == 0
+    assert "{on,off,succeeded,failed,inactive}" in result.output
