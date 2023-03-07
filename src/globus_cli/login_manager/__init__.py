0cfa24315a63b6646f31ba04084e5a2701be59b1
Stephen Rosen <sirosen@globus.org>
2023-03-07 16:22:56 -0500

    Update src/globus_cli/login_manager/__init__.py

--- src/globus_cli/login_manager/__init__.py
+++ src/globus_cli/login_manager/__init__.py
@@ -15,7 +15,6 @@ from .utils import is_remote_session
 
 __all__ = [
     "MissingLoginError",
-    "compute_timer_scope",
     "is_remote_session",
     "LoginManager",
     "delete_templated_client",
