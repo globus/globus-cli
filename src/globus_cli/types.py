61c1ab12d327eece38ebdb988cae0496f94e912f
Derek Schlabach <derek@globus.org>
2025-02-03 15:21:52 -0600

    Max recursion depth in json value typeguard

--- src/globus_cli/types.py
+++ src/globus_cli/types.py
@@ -42,18 +42,58 @@ JsonValue: TypeAlias = t.Union[int, float, str, bool, None, JsonList, JsonDict]
 
 
 def is_json_value(value: t.Any) -> TypeGuard[JsonValue]:
+    """
+    A typeguard for JsonValue.
+    If the JsonValue is > 100 levels nested deep, it will be considered a JsonValue.
+    """
+    return _is_json_value(value, max_depth=100)
+
+
+def _is_json_value(value: t.Any, max_depth: int) -> TypeGuard[JsonValue]:
+    if max_depth == 0:
+        return True
+
     if isinstance(value, (int, float, str, bool, type(None))):
         return True
-    return is_json_list(value) or is_json_dict(value)
+    depth = max_depth - 1
+    return _is_json_list(value, max_depth=depth) or _is_json_dict(
+        value, max_depth=depth
+    )
 
 
 def is_json_dict(value: t.Any) -> TypeGuard[JsonDict]:
+    """
+    A typeguard for JsonDict.
+    If the JsonDict is > 100 levels nested deep, it will be considered a JsonDict.
+    """
+    return _is_json_dict(value, max_depth=100)
+
+
+def _is_json_dict(value: t.Any, max_depth: int) -> TypeGuard[JsonDict]:
+    if max_depth == 0:
+        return True
+
     if not isinstance(value, dict):
         return False
-    return all(isinstance(k, str) and is_json_value(v) for k, v in value.items())
+    depth = max_depth - 1
+    return all(
+        isinstance(k, str) and _is_json_value(v, max_depth=depth)
+        for k, v in value.items()
+    )
 
 
 def is_json_list(value: t.Any) -> TypeGuard[JsonList]:
+    """
+    A typeguard for JsonList.
+    If the JsonList is > 100 levels nested deep, it will be considered a JsonList.
+    """
+    return _is_json_list(value, max_depth=100)
+
+
+def _is_json_list(value: t.Any, max_depth: int) -> TypeGuard[JsonList]:
+    if max_depth == 0:
+        return True
+
     if not isinstance(value, list):
         return False
     return all(is_json_value(item) for item in value)
