e78defb5ca431b7b1a9a4210b92c4917888b4bac
Charlie Whitehead <charlie@globus.org>
2025-08-13 16:07:50 -0400

    Add `revoke-subscription-verification` option to `group update` command

--- src/globus_cli/commands/group/update.py
+++ src/globus_cli/commands/group/update.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import typing as t
 import uuid
 
 import click
@@ -18,6 +19,11 @@ from ._common import group_id_arg
 @click.option(
     "--terms-and-conditions", help="Terms and conditions for group membership"
 )
+@click.option(
+    "--revoke-subscription-verification",
+    is_flag=True,
+    help="Flag to remove the subscription admin verification from the group.",
+)
 @LoginManager.requires_login("groups")
 def group_update(
     login_manager: LoginManager,
@@ -26,6 +32,7 @@ def group_update(
     name: str | None,
     description: str | None,
     terms_and_conditions: str | None,
+    revoke_subscription_verification: bool,
 ) -> None:
     """Update an existing group."""
     groups_client = login_manager.get_groups_client()
@@ -36,7 +43,7 @@ def group_update(
     # assemble put data using existing values for any field not given
     # note that the API only allows modification of certain fields
     #   https://groups.api.globus.org/redoc#tag/groups/operation/update_group_v2_groups__group_id__put
-    data = {}
+    data: dict[str, t.Any] = {}
     for attrname, argval in (
         ("name", name),
         ("description", description),
@@ -46,6 +53,10 @@ def group_update(
             data[attrname] = argval
         else:
             data[attrname] = group[attrname]
+    if revoke_subscription_verification:
+        data["subscription_admin_verified_id"] = None
+    else:
+        data["subscription_admin_verified_id"] = group["subscription_admin_verified_id"]
 
     response = groups_client.update_group(group_id, data)
 
