3eb00f612a2763f34a8b1cd1d23c0ca735c210d2
Stephen Rosen <sirosen@globus.org>
2025-12-29 12:39:44 -0600

    Remove globus_cli_rmtree from toxfile
    This is no longer used for any tox environments. Remove it.

--- toxfile.py
+++ toxfile.py
@@ -12,28 +12,14 @@ We only implement hooks which we need.
 from __future__ import annotations
 
 import logging
-import pathlib
-import shutil
 import typing as t
 
 from tox.plugin import impl
 
 if t.TYPE_CHECKING:
-    from tox.config.sets import EnvConfigSet
-    from tox.session.state import State
     from tox.tox_env.api import ToxEnv
 
 log = logging.getLogger(__name__)
-
-
-@impl
-def tox_add_env_config(env_conf: EnvConfigSet, state: State) -> None:
-    env_conf.add_config(
-        keys=["globus_cli_rmtree"],
-        of_type=list[str],
-        default=[],
-        desc="A dir tree to remove before running the environment commands",
-    )
 
 
 @impl
@@ -46,10 +32,3 @@ def tox_before_run_commands(tox_env: ToxEnv) -> None:
         # -- 0 means tests will run in the main process, not even in a worker
         setenv = tox_env.conf.load("set_env")
         setenv.update({"TOX_PARALLEL": "1", "PYTEST_XDIST_AUTO_NUM_WORKERS": "0"})
-
-    cli_rmtree = tox_env.conf.load("globus_cli_rmtree")
-    for name in cli_rmtree:
-        path = pathlib.Path(name)
-        if path.exists():
-            log.warning(f"globus_cli_rmtree: {path}")
-            shutil.rmtree(path)
