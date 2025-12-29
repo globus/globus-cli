3f558d26f615635d0d550fa78de8737f0a088796
Stephen Rosen <sirosen@globus.org>
2025-12-29 17:40:49 -0600

    Add lines to make tox output grouped in GitHub
    Group the outputs so that they are easier to navigate.

--- toxfile.py
+++ toxfile.py
@@ -11,15 +11,64 @@ We only implement hooks which we need.
 
 from __future__ import annotations
 
+import contextvars
 import logging
+import os
 import typing as t
 
 from tox.plugin import impl
 
 if t.TYPE_CHECKING:
+    from tox.execute import Outcome
     from tox.tox_env.api import ToxEnv
 
 log = logging.getLogger(__name__)
+
+
+GH_GROUP: contextvars.ContextVar[str | None] = contextvars.ContextVar(
+    "GH_GROUP", default=None
+)
+
+
+def on_gh_actions() -> bool:
+    return os.getenv("GITHUB_ACTIONS") == "true"
+
+
+def gh_group_start(name: str) -> None:
+    if on_gh_actions():
+        # when starting a group, close any previous group
+        # needed for install -> run
+        #
+        # also make sure it is "reentry" safe, since on_install fires multiple times
+        # (see detailed note below)
+        if prior_group := GH_GROUP.get():
+            if prior_group != name:
+                print("::endgroup::")
+                print(f"::group::tox:{name}")
+                GH_GROUP.set(name)
+        else:
+            print(f"::group::tox:{name}")
+            GH_GROUP.set(name)
+
+
+def gh_group_end() -> None:
+    if on_gh_actions():
+        print("::endgroup::")
+        GH_GROUP.set(None)
+
+
+@impl
+def tox_on_install(
+    tox_env: ToxEnv, arguments: t.Any, section: str, of_type: str
+) -> None:
+    # the install block is generic in name so that the name does not change as this hook
+    # fires multiple times while tox is spinning up a new environment
+    # experimentally, we see
+    # - env specific on_install
+    # - env specific on_install (again?!)
+    # - .pkg on_install
+    # - env specific on_install again
+    gh_group_start("install")
 
 
 @impl
@@ -32,3 +81,12 @@ def tox_before_run_commands(tox_env: ToxEnv) -> None:
         # -- 0 means tests will run in the main process, not even in a worker
         setenv = tox_env.conf.load("set_env")
         setenv.update({"TOX_PARALLEL": "1", "PYTEST_XDIST_AUTO_NUM_WORKERS": "0"})
+
+    gh_group_start(f"{tox_env.name}:run")
+
+
+@impl
+def tox_after_run_commands(
+    tox_env: ToxEnv, exit_code: int, outcomes: list[Outcome]
+) -> None:
+    gh_group_end()
