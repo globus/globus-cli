7e0d5aef1d4b7892fab227da468625efd8d194fd
m1yag1 <8730430+m1yag1@users.noreply.github.com>
2026-05-14 14:35:49 -0500

    fixup! Added globus flows registered-api show command

--- src/globus_cli/commands/flows/registered_api/show.py
+++ src/globus_cli/commands/flows/registered_api/show.py
@@ -11,23 +11,15 @@ from globus_cli.termio import display
 
 
 @command("show")
-@click.argument("registered_api_id")
+@click.argument("registered_api_id", type=click.UUID)
 @LoginManager.requires_login("auth", "flows")
-def show_command(login_manager: LoginManager, *, registered_api_id: str) -> None:
+def show_command(login_manager: LoginManager, *, registered_api_id: uuid.UUID) -> None:
     """
     Show a registered API.
-
-    Accepts a registered API UUID.
     """
     flows_client = login_manager.get_flows_client()
 
-    # Convert string to UUID if it's a valid UUID
-    try:
-        api_id: uuid.UUID | str = uuid.UUID(registered_api_id)
-    except ValueError:
-        api_id = registered_api_id
-
-    res = flows_client.get_registered_api(api_id)
+    res = flows_client.get_registered_api(registered_api_id)
 
     auth_client = login_manager.get_auth_client()
     fields = registered_api_format_fields(auth_client, res.data)
