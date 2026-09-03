76867dee4a6dd1df4ddfd01ea8789c148bf7847b
Kurt McKee <contactme@kurtmckee.org>
2026-09-03 10:37:20 -0500

    Make CLI completion generation dynamic, not hard-coded

--- src/globus_cli/parsing/shell_completion.py
+++ src/globus_cli/parsing/shell_completion.py
@@ -4,113 +4,14 @@ import os
 import typing as t
 
 import click
-
-# Tag: click-updates-note
-# When click versions are updated, re-run the CLI-completion generator commands for:
-#
-# BASH_SHELL_COMPLETER
-# FISH_SHELL_COMPLETER
-# ZSH_SHELL_COMPLETER
-#
+from click.shell_completion import (
+    BashComplete,
+    FishComplete,
+    ShellComplete,
+    ZshComplete,
+)
 
 C = t.TypeVar("C", bound=t.Union[t.Callable[..., t.Any], click.Command])
-
-# Output collected by running `_GLOBUS_COMPLETE=bash_source globus`
-BASH_SHELL_COMPLETER = r"""
-_globus_completion() {
-    local IFS=$'\n'
-    local response
-
-    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _GLOBUS_COMPLETE=bash_complete $1)
-
-    for completion in $response; do
-        IFS=',' read type value <<< "$completion"
-
-        if [[ $type == 'dir' ]]; then
-            COMPREPLY=()
-            compopt -o dirnames
-        elif [[ $type == 'file' ]]; then
-            COMPREPLY=()
-            compopt -o default
-        elif [[ $type == 'plain' ]]; then
-            COMPREPLY+=($value)
-        fi
-    done
-
-    return 0
-}
-
-_globus_completion_setup() {
-    complete -o nosort -F _globus_completion globus
-}
-
-_globus_completion_setup;
-"""  # noqa: E501
-
-# Output collected by running `_GLOBUS_COMPLETE=fish_source globus`
-FISH_SHELL_COMPLETER = r"""
-function _globus_completion;
-    set -l response (env _GLOBUS_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) globus);
-
-    for completion in $response;
-        set -l metadata (string split "," $completion);
-
-        if test $metadata[1] = "dir";
-            __fish_complete_directories $metadata[2];
-        else if test $metadata[1] = "file";
-            __fish_complete_path $metadata[2];
-        else if test $metadata[1] = "plain";
-            echo $metadata[2];
-        end;
-    end;
-end;
-
-complete --no-files --command globus --arguments "(_globus_completion)";
-"""  # noqa: E501
-
-# Output collected by running `_GLOBUS_COMPLETE=zsh_source globus`
-ZSH_SHELL_COMPLETER = r"""
-#compdef globus
-
-_globus_completion() {
-    local -a completions
-    local -a completions_with_descriptions
-    local -a response
-    (( ! $+commands[globus] )) && return 1
-
-    response=("${(@f)$(env COMP_WORDS="${words[*]}" COMP_CWORD=$((CURRENT-1)) _GLOBUS_COMPLETE=zsh_complete globus)}")
-
-    for type key descr in ${response}; do
-        if [[ "$type" == "plain" ]]; then
-            if [[ "$descr" == "_" ]]; then
-                completions+=("$key")
-            else
-                completions_with_descriptions+=("$key":"$descr")
-            fi
-        elif [[ "$type" == "dir" ]]; then
-            _path_files -/
-        elif [[ "$type" == "file" ]]; then
-            _path_files -f
-        fi
-    done
-
-    if [ -n "$completions_with_descriptions" ]; then
-        _describe -V unsorted completions_with_descriptions -U
-    fi
-
-    if [ -n "$completions" ]; then
-        compadd -U -V unsorted -a completions
-    fi
-}
-
-if [[ $zsh_eval_context[-1] == loadautofunc ]]; then
-    # autoload from fpath, call function directly
-    _globus_completion "$@"
-else
-    # eval/source/. command, register function for later
-    compdef _globus_completion globus
-fi
-"""  # noqa: E501
 
 
 def print_completer_option(f: C) -> C:
@@ -121,29 +22,26 @@ def print_completer_option(f: C) -> C:
             return
 
         if value == "BASH":
-            detected = "bash"
+            cls: type[ShellComplete] = BashComplete
         elif value == "FISH":
-            detected = "fish"
+            cls = FishComplete
         elif value == "ZSH":
-            detected = "zsh"
+            cls = ZshComplete
         else:  # auto
-            detected = "bash"  # default to bash completion
-            if "SHELL" in os.environ:  # see if shell matches, e.g. `/bin/zsh`
+            cls = BashComplete  # Default to bash completion.
+            if "SHELL" in os.environ:
                 if os.environ["SHELL"].endswith("zsh"):
-                    detected = "zsh"
-            elif "FISH_VERSION" in os.environ:
-                detected = "fish"
+                    cls = ZshComplete
 
-        if detected == "bash":
-            click.echo(BASH_SHELL_COMPLETER)
-        elif detected == "fish":
-            click.echo(FISH_SHELL_COMPLETER)
-        elif detected == "zsh":
-            click.echo(ZSH_SHELL_COMPLETER)
-        else:
-            raise NotImplementedError("Unsupported shell completion")
-
-        click.get_current_context().exit(0)
+        root_ctx = ctx.find_root()
+        completer = cls(
+            cli=root_ctx.command,
+            ctx_args={},
+            prog_name="globus",
+            complete_var="_GLOBUS_COMPLETE",
+        )
+        click.echo(completer.source())
+        ctx.exit(0)
 
     def _compopt(flag: str, value: str) -> t.Callable[[C], C]:
         return click.option(
